39f7d7e70add0467dfbaaee4d34cd9b423a38c61
Dakodah Spurlin <dcspurlin@gmail.com>
2024-10-07 17:02:31 -0500

    Linked both the CSS and JS files to the HTML file and started working with Arrays

---
+++ array.js
@@ -0,0 +1,5 @@
+console.log('Hello World!');
+let sandwitch = [ 'bacon', 'salami', 'pepper' ]
+// Worked on manipulating the array in browser using commands such as: Shift, Unshift, Push, and Pop.
+
+
